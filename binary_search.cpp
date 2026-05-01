902b71330f28b704bce8cd4804841f0616295e04
nitin <nitingangwar667@gmail.com>
2026-05-02 02:31:49 +0530

    binary search

---
+++ binary_search.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int st = 0, end = nums.size() - 1;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;   
+            if (nums[mid] == target) {
+                return mid;
+            }    
+            if (nums[st] <= nums[mid]) {
+                if (target >= nums[st] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    st = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
+int main() {
+    Solution solution;
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+    int result = solution.search(nums, target);
+    cout << "Target found at index: " << result << endl;
+    return 0;
+}
