dd35e209bb143ad997c6f9340d56bfd3d0b84776
nitin <nitingangwar667@gmail.com>
2026-05-07 21:55:37 +0530

    sort 0-1-2

---
+++ sort_012.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void sort012(vector<int>& arr) {
+        int low = 0, mid = 0, high = arr.size() - 1;
+
+        while (mid <= high) {
+            if(arr[mid]==0){
+                swap(arr[low],arr[mid]);
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                swap(arr[mid],arr[high]);
+                high--;
+            }
+        }
+    }
+};
+int main() {
+    Solution sol;
+    vector<int> arr = {0, 1, 2, 0, 1, 2,0,0,0,1,1,1,2,2,2};
+    sol.sort012(arr);
+    
+    cout << "Sorted array: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
