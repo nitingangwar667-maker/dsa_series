3d255cd5b7d9ab4f487f55a2bc942345add70534
nitin <nitingangwar667@gmail.com>
2026-05-07 21:59:11 +0530

    sort0-1-2

--- sort_012.cpp
+++ sort_012.cpp
@@ -26,7 +26,6 @@ int main() {
     Solution sol;
     vector<int> arr = {0, 1, 2, 0, 1, 2,0,0,0,1,1,1,2,2,2};
     sol.sort012(arr);
-
     cout << "Sorted array: ";
     for (int num : arr) {
         cout << num << " ";
