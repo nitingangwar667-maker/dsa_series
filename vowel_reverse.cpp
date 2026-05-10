04f69e832570e1d5151b2597b26c0a67f6e3f234
nitin <nitingangwar667@gmail.com>
2026-05-10 14:05:51 +0530

    vowel swap

---
+++ vowel_reverse.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+class Solution {
+public:
+    bool isVowel(char c) {
+        c = tolower(c);
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+    }
+
+    string reverseVowels(string s) {
+        int st = 0, end = s.size() - 1;
+        while (st < end) {
+            if (!isVowel(s[st])) {
+                st++;
+            } else if (!isVowel(s[end])) {
+                end--;
+            } else {
+                swap(s[st], s[end]);
+                st++;
+                end--;
+            }
+        }
+        return s;
+    }
+};
+int main() {
+    Solution sol;
+    string s = "hello";
+    cout << sol.reverseVowels(s) << endl; // Output: "holle"
+    return 0;
+}
